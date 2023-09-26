5be2c736905c02c7468f8e149b5dbef5c6817569
Jefferson Ccolque <jccolquequ@unsa.edu.pe>
2023-09-26 11:50:38 -0500

    Proyecto

---
+++ src/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+
+    String name;
+
+    public void setName(String n){
+        this.name = n;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+}
