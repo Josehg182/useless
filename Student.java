dd0521dcfda6f56caba25e3cdc34f3adbccbae67
Jefferson Ccolque <jccolquequ@unsa.edu.pe>
2023-09-27 21:35:04 -0500

    actualizacion

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String name;
+
+    public void setName(String n){
+        this.name = n;
+    }
+    //////cambio
+    
+    public String getName(){
+        return this.name;
+    }
+}
